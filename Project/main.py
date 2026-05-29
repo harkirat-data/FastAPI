4daab7f28d6d7d2e708b1d8c0ebde09644e57a29
harkirat-data <harkiratsingh96kk@gmail.com>
2026-05-29 16:31:45 +0530

    Added query parameters

--- Project/main.py
+++ Project/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path , HTTPException 
+from fastapi import FastAPI, Path, Query, HTTPException 
 import json
 
 app = FastAPI()
@@ -28,3 +28,22 @@ def view_patient(patient_id : str = Path(..., description = 'ID of the patient i
     if patient_id in data:
         return data[patient_id]
     raise HTTPException(status_code = 404, detail = 'Patient not found')
+
+@app.get('/sort')
+def sort_patients(sort_by : str = Query(..., description = 'Sort on the basis of height, weight or bmi'), 
+                  order : str = Query('asc', description = 'Sort in ascending or descending order')):
+    
+    valid_fields = ['height', 'weight', 'bmi']
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code = 400, detail = f'Invalid field selection from {valid_fields}')
+
+    if order not in ['asc' , 'desc']:
+        raise HTTPException(status_code = 400, detail = 'Invalid order selection between asc and desc')
+
+    data = load_data()
+        
+    sort_order = True if order == 'desc' else False
+    sorted_data = sorted(data.values(), key = lambda x : x[sort_by], reverse = sort_order)
+
+    return sorted_data
+    
